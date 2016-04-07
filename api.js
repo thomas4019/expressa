13d9187b224a93c03888d5bf45c7edb8f4e0257c
Thomas Hansen <th4019@gmail.com>
2016-04-06 19:32:50 -0600

    database fixes

--- api.js
+++ api.js
@@ -10,6 +10,8 @@ var conString = require('./config.js').getConnectionURL()
 var auth = require('./auth')
 var path = require('path')
 var fs = require('fs')
+var mongoQuery = require('mongo-query');
+var randomstring = require("randomstring");
 
 var ajv = require('ajv')({allErrors: true});
 
@@ -19,6 +21,7 @@ schemaValidators = {};
 
 for (var schemaName in schemas) {
 	var schema = schemas[schemaName]
+	//console.log('compiling ' + schemaName);
 	schemaValidators[schemaName] = ajv.compile(schema)
 }
 
@@ -43,14 +46,15 @@ storage = {
 	post: 'postgres',
 	role: 'file',
 	users: 'postgres',
-	schemas: 'file'	
+	schemas: 'file',
+	collection: 'file'
 }
 fileDbs = {
 
 }
 for (schema in storage) {
 	if (storage[schema] == 'file') {
-		fileDbs[schema] = new Store('data/' + schema)
+		fileDbs[schema] = new Store('data/' + schema, {pretty: true})
 	}
 }
 
@@ -59,10 +63,10 @@ pg.connect(conString, function(err, client, done) {
 		console.error(err);
 	}
 
-	/*router.get('/:schema/schema', function (req, res) {
+	router.get('/:schema/schema', function (req, res) {
 		var schema = schemaDb.getSync(req.params.schema)
 		res.send(schema)
-	})*/
+	})
 
 	router.get('/:schema/index', function (req, res) {
 		if (storage[req.params.schema] == 'file') {
@@ -71,8 +75,12 @@ pg.connect(conString, function(err, client, done) {
 			})
 		} else {
 			client.query('SELECT * FROM ' + req.params.schema, function(err, result) {
-		        res.send(result.rows)
-	      	})
+				var out = {};
+				result.rows.forEach(function(row) {
+					out[row.id] = row.data;
+				});
+				res.send(out)
+			})
 		}
 	})
 
@@ -103,13 +111,27 @@ pg.connect(conString, function(err, client, done) {
 			return res.status(500).send(schemaValidators[req.params.schema].errors);
 		}
 		if (storage[req.params.schema] == 'file') {
-
+			var id = randomstring.generate(8);
+			fileDbs[req.params.schema].save(id, data, function(err, data) {
+				if (err) {
+					res.status(500).send(err);
+				} else {
+					res.send({
+						status: 'OK',
+						id: id
+					});
+				}
+			})
 		} else {
-			client.query('INSERT INTO '+req.params.schema+' (data) VALUES ($1)', [data], function(err, result) {
+			client.query('INSERT INTO '+req.params.schema+' (data) VALUES ($1) RETURNING id;', [data], function(err, result) {
 				if (err) {
 					console.error(err);
 				}
-				res.send('OK')	
+				var id = result.rows[0].id;
+				res.send({
+					status: 'OK',
+					id: id
+				})
 			})
 		}
 		
@@ -118,7 +140,6 @@ pg.connect(conString, function(err, client, done) {
 	router.get('/:schema/:id', function (req, res) {
 		if (storage[req.params.schema] == 'file') {
 			fileDbs[req.params.schema].get(req.params.id, function(err, data) {
-
 				res.send(data);
 			})
 		} else {
@@ -129,19 +150,19 @@ pg.connect(conString, function(err, client, done) {
 				if (result.rowCount == 0) {
 					return res.status(404).send('document not found');
 				}
-		        res.send(result.rows[0])
+		        res.send(result.rows[0].data)
 	      	})
 		}
 	})
 
-	router.put('/:schema/:id', function (req, res) {
+	var update = function (req, res, successCallback) {
 		var data = req.body
 		var valid = schemaValidators[req.params.schema](data);
 		if (!valid) {
 			return res.status(500).send(schemaValidators[req.params.schema].errors);
 		}
 		if (storage[req.params.schema] == 'file') {
-			fileDbs[req.params.schema].set(req.params.id, data, function(err, data) {
+			fileDbs[req.params.schema].save(req.params.id, data, function(err, data) {
 				if (err) {
 					res.status(500).send(err);
 				} else {
@@ -156,7 +177,40 @@ pg.connect(conString, function(err, client, done) {
 				if (result.rowCount == 0) {
 					return res.status(404).send('document not found');
 				}
-				res.send('OK')	
+				successCallback();
+			})
+		}
+	};
+
+	router.put('/:schema/:id', function(req, res) {
+		update(req, res, function() {
+			res.send('OK');
+		});
+	})
+
+	router.post('/:schema/:id/update', function (req, res) {
+		var modifier = req.body
+
+		if (storage[req.params.schema] == 'file') {
+			// TODO
+			res.send('TODO');
+		} else {
+			client.query('SELECT * FROM ' + req.params.schema + ' WHERE id = $1', [req.params.id], function(err, result) {
+				if (err) {
+					return res.status(500).send(err);
+				}
+				if (result.rowCount == 0) {
+					return res.status(404).send('document not found');
+				}
+				var doc = result.rows[0].data
+
+				var changes = mongoQuery(doc, {}, modifier);
+
+				req.body = doc;
+
+				update(req, res, function() {
+					res.send(doc);
+				});	
 			})
 		}
 	})
@@ -178,7 +232,7 @@ pg.connect(conString, function(err, client, done) {
 				if (result.rowCount == 0) {
 					return res.status(404).send('document not found');
 				}
-				res.send('OK')	
+				res.send('OK')
 			})
 		}
 	})
--- edit.js
+++ edit.js
@@ -27,15 +27,14 @@ pg.connect(conString, function(err, client, done) {
 	router.use('/node_modules', express.static(p+'/node_modules'))
 	router.use('/templates', express.static(p+'/templates'))
 
-
-	router.get('/:schema', function (req, res) {
-		var p = path.join(path.dirname(fs.realpathSync(__filename)), 'templates/edit.html')
+	router.get('/schema/:schema', function (req, res) {
+		var p = path.join(path.dirname(fs.realpathSync(__filename)), 'templates/schema.html')
 		var t = fs.readFileSync(p, "utf8")
 		res.send(t)
 	})
 
-	router.get('/schema/:schema', function (req, res) {
-		var p = path.join(path.dirname(fs.realpathSync(__filename)), 'templates/schema.html')
+	router.get('/:schema*', function (req, res) {
+		var p = path.join(path.dirname(fs.realpathSync(__filename)), 'templates/edit.html')
 		var t = fs.readFileSync(p, "utf8")
 		res.send(t)
 	})
