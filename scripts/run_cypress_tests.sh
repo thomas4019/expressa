0f960dc47e27dde8ae31625dc1211e10c278d272
Thomas Hansen <th4019@gmail.com>
2020-08-15 23:22:40 -0600

    Add cypress to CircleCI

--- scripts/run_cypress_tests.sh
+++ scripts/run_cypress_tests.sh
@@ -2,5 +2,6 @@ rm -rf testdata
 node test/testserver.js &
 pid=$!
 sleep 1
-./node_modules/.bin/cypress run
+./node_modules/.bin/cypress run --reporter junit \
+  --reporter-options "mochaFile=results/cypress-[hash].xml,toConsole=true"
 kill "$pid"
