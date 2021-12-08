1ccdbc63491f87af01be78c5ef0b1f35130f55c4
Kane Mason <kanemason@me.com>
2021-12-08 15:18:49 +0200

    update postgres count using sql count query

--- db/postgres.js
+++ db/postgres.js
@@ -35,8 +35,18 @@ module.exports = function (settings, collectionId, collection) {
       const result = await pool.query(query)
       return result.rows.map((row) => row.data)
     },
-    count: async function(...params) {
-      return (await this.find(...params)).length
+    count: async function(rawQuery, offset, limit) {
+      const arrayFields = util.getArrayPaths('', collection.schema)
+      const pgQuery = mongoToPostgres('data', rawQuery || {}, arrayFields)
+      let query = 'SELECT COUNT(*) FROM ' + collectionId + (pgQuery ? ' WHERE ' + pgQuery : '')
+      if (typeof offset !== 'undefined') {
+        query += ' OFFSET ' + offset
+      }
+      if (typeof limit !== 'undefined') {
+        query += ' LIMIT ' + limit
+      }
+      const result = await pool.query(query)
+      return parseInt(result.rows[0].count)
     },
     get: async function (id, fields) {
       const arrayFields = util.getArrayPaths('', collection.schema)
