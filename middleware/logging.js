559eec38d4a772ebbffbcbbde6908c68f2e95a5f
Kane Mason <kanemason@me.com>
2023-07-27 12:47:59 +0200

    ensure requests using custom middleware log errors correctly

--- middleware/logging.js
+++ middleware/logging.js
@@ -4,6 +4,16 @@ const util = require('../util')
 
 module.exports = function logRequests(req, res, next) {
   if (req.db.requestlog) {
+    const oldSend = res.send
+    res.send = function(...params) {
+      const data = params[0]
+      // ensure requests using custom middleware log error messages
+      if (typeof data?.error === 'string') {
+        req.returnedError ??= new util.ApiError(res.statusCode, data.error)
+      }
+      res.send = oldSend // set function back to avoid the 'double-send'
+      return res.send(...params) // just call as normal with data
+    }
     onFinished(res, async function (err) {
       if (err) {
         console.error('error found while logging.')
